49b1fe2f258f2f897021011ca5972d62d7196f8a
Matteo Rizzi <s244853@studenti.polito.it>
2020-08-10 17:52:31 +0200

    Prova

--- src/main/java/it/polito/tdp/RizziMatteo/model/TestModel.java
+++ src/main/java/it/polito/tdp/RizziMatteo/model/TestModel.java
@@ -8,7 +8,7 @@ public class TestModel {
 	public static void main(String[] args) {
 		Model model = new Model();
 		
-		List<String> generiSelezionati = model.getMusicalGenres();
+		List<String> generiSelezionati = model.getMusicalGenres(); 
 
 		List<String> generiPrivilegiati = new ArrayList<>();
 		List<Artista> best = model.calcolaCombinazioneMigliore(400000, 3, generiSelezionati, generiPrivilegiati, 1.0, 'B');
